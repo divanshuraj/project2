ec06c05962329f30fa9931fe70361a7a728c4654
divanshuraj <101104941+divanshuraj@users.noreply.github.com>
2022-03-07 17:49:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ from flask import Flask,render_template,request
 app=Flask(__name__)
 @app.route('/')
 def xyz():
-     return render_template('sum2.html')
+     return render_template('app.html')
 app.route('data',methods=['POST'])
 def obj():
     num1=request.form['a']
     num2=request.form['b']
     total=int(num1)+int(num2)
-    return render_template('sum2.html',tanu=total)
+    return render_template('app.html',tanu=total)
 if __name__ == '__main__':
     app.run()    
 
