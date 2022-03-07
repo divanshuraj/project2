d77614ee6d8606d5b116580abeaaf6ee774893f1
divanshuraj <101104941+divanshuraj@users.noreply.github.com>
2022-03-07 18:11:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app=Flask(__name__)
 @app.route('/')
 def xyz():
      return render_template('app.html')
-app.route('data',methods=['POST'])
+app.route('/info',methods=['POST'])
 def obj():
      if(request.method=='POST'):
           num1=request.form['a']
           num2=request.form['b']
           total=int(num1)+int(num2)
-          return render_template('app.html',tanu=total)
+          return render_template('app.html',total1=total)
 if __name__ == '__main__':
     app.run()    
 
